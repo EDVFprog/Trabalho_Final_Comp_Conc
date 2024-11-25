14abb80f9c0ee0f111dd539cf361a092ccfb8757
Eduarda Fahr <78647206+EDVFprog@users.noreply.github.com>
2024-11-25 17:21:12 -0300

    Update TrabalhoFinal.c
    Atualizado para ser sensitive case

--- TrabalhoFinal.c
+++ TrabalhoFinal.c
@@ -24,8 +24,8 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond_producer = PTHREAD_COND_INITIALIZER;
 pthread_cond_t cond_consumer = PTHREAD_COND_INITIALIZER;
 
-const char *documentos[NUM_DOCUMENTS] = {"doc1.txt", "doc2.txt", "doc3.txt"};
-char *termo;
+const char *documentos[NUM_DOCUMENTS] = {"doc1_128.txt", "doc2_128.txt", "doc3_128.txt"};
+const char *termo;
 
 /* Resultados globais */
 int n_ocorrencias_termo[NUM_DOCUMENTS] = {0};
@@ -37,7 +37,7 @@ double TF(int num_ocorrencias, int num_palavras);
 double IDF(int num_docs_contendo_termo);
 int leitor_arquivo(const char *fonte, char buffer[]);
 void esvaziar_buffer(char buffer[]);
-void identificar(const char buffer[],  char termo[], int *n_ocorrencias_termo, int *n_palavras_doc, int *termo_encontrado_no_doc);
+void identificar(const char buffer[],  const char termo[], int *n_ocorrencias_termo, int *n_palavras_doc, int *termo_encontrado_no_doc);
 
 /* Threads */
 void *produtor(void *arg);
@@ -52,10 +52,14 @@ int main(int argc, char *argv[]) {
       printf("Uso: %s <termo>\n", argv[0]);
       return 1;
    }
-
-    
-    termo =argv[1];
- 
+    // Tratamento de string sensitive case
+    char  * string = argv[1];
+     for(int i = 0; string[i]; i++){
+      string[i] = tolower(string[i]);
+  
+  }
+  termo = string;
+  
     
    
 
@@ -85,7 +89,6 @@ int main(int argc, char *argv[]) {
    for (int i = 0; i < NUM_DOCUMENTS; i++) {
     	double tf = TF(n_ocorrencias_termo[i], n_palavras_doc[i]);
    	double tf_idf = idf*tf;
-   	fprintf(output, "%d", n_ocorrencias_termo[i]);
         fprintf(output,"Documento %d: TF = %.4f\n", i + 1, tf);
         fprintf(output,"Documento %d: TF-IDF = %.4f\n", i + 1, tf_idf);
         
@@ -132,10 +135,15 @@ void esvaziar_buffer(char buffer[]) {
     }
 }
 
-void identificar(const char buffer[], char termo[], int *n_ocorrencias_termo, int *n_palavras_doc, int *termo_encontrado_no_doc) {
+void identificar(const char buffer[], const char termo[], int *n_ocorrencias_termo, int *n_palavras_doc, int *termo_encontrado_no_doc) {
     int termo_presente_no_doc = FALSE;
-    const char *p = buffer;
-
+    // Tratamento de string sensitive case
+    char *texto = buffer;
+    for(int i = 0; texto[i]; i++){
+      texto[i] = tolower(texto[i]);
+    }
+    const char *p = texto;
+    
     while (*p != '\0') {
         // Ignora espaços e quebras de linha
         while (*p == ' ' || *p == '\n') p++; 
